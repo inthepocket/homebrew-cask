e429076f39e20b38283c4162979ba788a56d1eff
Yves Delcoigne <yves.delcoigne@inthepocket.mobi>
2019-08-09 13:38:45 +0200

    Update xamarin-ios from 12.8.0.2 to 12.14.0.114

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-ios' do
-  version '12.8.0.2'
-  sha256 'b9d1d8f9d3732a57b8b1365f0ef4e7b9de6327125f3c08a1b9db3e954d0b6749'
+  version '12.14.0.114'
+  sha256 '5d00d7731392905e06170a8675c706907dfc4e0860523c5b5241acfbd5cd49c7'
 
   url "https://dl.xamarin.com/MonoTouch/Mac/xamarin.ios-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
